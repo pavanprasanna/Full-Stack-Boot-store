4ca7d14649501629a90b0f6836eab4c15e711a24
Pavan P <pavanp@altoura.com>
2024-10-30 20:25:10 +0530

    commit with lib

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+    <nav></nav>
+    <Outlet/>
+    <footer></footer>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/routers/routers.jsx
@@ -0,0 +1,27 @@
+import {
+    createBrowserRouter
+} from "react-router-dom";
+import App from "../App";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element:<div>Home</div>,
+            },
+            {
+                path: "/orders",
+                element: <div>Orders</div>,
+            },
+            {
+                path: "/about",
+                element: <div>About</div>,
+            }
+        ]
+    },
+]);
+
+export default router;
