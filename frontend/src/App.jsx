6471da491229045e06466abb01560faff92b5c71
Pavan P <pavanp@altoura.com>
2024-10-30 21:12:11 +0530

    added css for app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,10 @@ function App() {
   return (
     <>
     <nav></nav>
+    <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
+
     <Outlet/>
+    </main>
     <footer></footer>
     </>
   )
